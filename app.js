fcc09518a049fbf652f36fef6483169b7d44de12
LPayyapilli <luke.payyapilli@gmail.com>
2015-08-02 15:39:31 -0400

    adds route and app file for messages

--- app.js
+++ app.js
@@ -41,8 +41,8 @@ app.use('/user', userRoutes);
 var spaceRoutes = require('./routes/spaces.js');
 app.use('/space', spaceRoutes);
 
-var messageRoutes = require('./routes/messages.js');
-app.use('/message', messageRoutes);
+var deleteRoutes = require('./routes/delete.js');
+app.use('/delete', deleteRoutes);
 
 
 // catch 404 and forward to error handler
